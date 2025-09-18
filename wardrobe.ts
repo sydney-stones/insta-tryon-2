530c9d477547a028710c7c3028b24fb256d0ed42
Sydney <syd.stones@gmail.com>
2025-09-18 19:23:16 +0100

    outfit update

--- wardrobe.ts
+++ wardrobe.ts
@@ -8,8 +8,68 @@ import { WardrobeItem } from './types';
 // Default wardrobe items hosted for easy access
 export const defaultWardrobe: WardrobeItem[] = [
   {
-    id: 'country',
-    name: 'Country',
-    url: 'https://raw.githubusercontent.com/sydney-stones/insta-tryon-2/main/country.png',
+    id: 'GameFair 1',
+    name: 'GameFair 1',
+    url: 'https://raw.githubusercontent.com/sydney-stones/insta-tryon-2/main/Outfits/GameFair-1.png',
+  },
+  {
+    id: 'GameFair 2',
+    name: 'GameFair 2',
+    url: 'https://raw.githubusercontent.com/sydney-stones/insta-tryon-2/main/Outfits/GameFair-2.png',
+  },
+  {
+    id: 'GameFair 3',
+    name: 'GameFair 3',
+    url: 'https://raw.githubusercontent.com/sydney-stones/insta-tryon-2/main/Outfits/GameFair-3.png',
+  },
+  {
+    id: 'GameFair 4',
+    name: 'GameFair 4',
+    url: 'https://raw.githubusercontent.com/sydney-stones/insta-tryon-2/main/Outfits/GameFair-4.png',
+  },
+  {
+    id: 'GameFair 5',
+    name: 'GameFair 5',
+    url: 'https://raw.githubusercontent.com/sydney-stones/insta-tryon-2/main/Outfits/GameFair-5.png',
+  },
+  {
+    id: 'GameFair 6',
+    name: 'GameFair 6',
+    url: 'https://raw.githubusercontent.com/sydney-stones/insta-tryon-2/main/Outfits/GameFair-6.png',
+  },
+  {
+    id: 'Silverstone-Alpine',
+    name: 'Silverstone-Alpine',
+    url: 'https://raw.githubusercontent.com/sydney-stones/insta-tryon-2/main/Outfits/Silverstone-Alpine.png',
+  },
+  {
+    id: 'Silverstone-RedBull',
+    name: 'Silverstone-RedBull',
+    url: 'https://raw.githubusercontent.com/sydney-stones/insta-tryon-2/main/Outfits/Silverstone-RedBull.png',
+  },
+  {
+    id: 'Silverstone-Mercedes',
+    name: 'Silverstone-Mercedes',
+    url: 'https://raw.githubusercontent.com/sydney-stones/insta-tryon-2/main/Outfits/Silverstone-Mercedes.png',
+  },
+  {
+    id: 'Silverstone-Ferrari',
+    name: 'Silverstone-Ferrari',
+    url: 'https://raw.githubusercontent.com/sydney-stones/insta-tryon-2/main/Outfits/Silverstone-Ferrari.png',
+  },
+  {
+    id: 'Silverstone-McLaren',
+    name: 'Silverstone-McLaren',
+    url: 'https://raw.githubusercontent.com/sydney-stones/insta-tryon-2/main/Outfits/Silverstone-McLaren.png',
+  },
+  {
+    id: 'Silverstone-AstonMartin',
+    name: 'Silverstone-AstonMartin',
+    url: 'https://raw.githubusercontent.com/sydney-stones/insta-tryon-2/main/Outfits/Silverstone-AstonMartin.png',
+  },
+  {
+    id: 'Silverstone-Williams',
+    name: 'Silverstone-Williams',
+    url: 'https://raw.githubusercontent.com/sydney-stones/insta-tryon-2/main/Outfits/Silverstone-Williams.png',
   }
 ];
